d84b5af3db1d4ab1abe2bf1b781bc34062a9bde9
Angel Poveda <saratoga258@gmail.com>
2018-11-09 16:36:30 -0500

    Primer carga del proyecto de pruebas iniciales

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Pruebas
+{
+    class Rectangulo
+    {
+		//Variables de un rectangulo
+		double Alto;
+		double Largo;
+
+		public void DefinirVariables(){
+			Alto = 4.5;
+			Largo = 3.5;
+		}
+
+		public double SacarArea(){
+			return Alto*Largo;
+		}
+
+		public void Mostrar(){
+			Console.WriteLine("Alto: " + Alto);
+			Console.WriteLine("Largo: " + Largo);
+			Console.WriteLine("Area: " + SacarArea());
+		}
+		/*
+		public void Leer(){
+			Console.WriteLine("Escribe algo");
+			String a = Console.ReadLine();
+			Console.WriteLine(a);
+		}*/
+	}
+	public class EjecucionRectangulo{
+		public static void Main(string[] args){
+			Rectangulo R = new Rectangulo();
+			R.DefinirVariables();
+			R.Mostrar();
+			//R.Leer();
+		}
+	}
+}
